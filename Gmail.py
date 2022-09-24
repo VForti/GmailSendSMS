2edc4a627efabb44c6d31d30aca5992a1b7ea6ea
VForti <99172698+VForti@users.noreply.github.com>
2022-09-24 17:53:09 +0300

    Gmail.py

---
+++ Gmail.py
@@ -0,0 +1,32 @@
+import smtplib
+import os
+from email.mime.text import MIMEText
+import random
+import string
+
+def send_email(message):
+    sender = "SENDER SMS"
+    password = "PASSWORD"
+    author = 'AUTHOR SMS'
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+
+    try:
+        server.login(sender, password)
+        msg = MIMEText(message)
+        msg["Subject"] = "SUBJECT"
+        server.sendmail(sender, author, msg.as_string())
+
+        # server.sendmail(sender, sender, f"Subject: CLICK ME PLEASE!\n{message}")
+
+        return "The message was sent successfully!"
+    except Exception as _ex:
+        return f"{_ex}\nCheck your login or password please!"
+
+
+def main():
+    message = ('TEXT')
+
+
+if __name__ == "__main__":
+    main()
